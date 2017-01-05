d49c2c49964f3b20a3c6aed90e4cad99695dbb9e
iory <ab.ioryz@gmail.com>
2017-01-06 02:05:14 +0900

    Using standard elisp boilerplate structure

--- launch-mode.el
+++ launch-mode.el
@@ -1,11 +1,29 @@
 ;;; launch-mode.el --- Major mode for launch-formatted text -*- lexical-binding: t; -*-
 
 ;; Author: iory <ab.ioryz@gmail.com>
-;; Maintainer: iory <ab.ioryz@gmail.com>
-;; Created: August 24, 2016
-;; Version: 0.0.1
-;; Keywords: ROS, launch file, GitHub
 ;; URL: https://github.com/iory/launch-mode
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.4") (helm "2.0"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; launch-mode provides major mode.
+;; And launch-mode provides goto-definition of other launch-files.
+
+;;; Code:
 
 (require 'launch-find-definition)
 
